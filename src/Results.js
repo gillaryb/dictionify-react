e849a85203406ef8363e8932854ffe9e32b388d7
Gilary Bacnis <gilarybacnis@gmail.com>
2023-06-01 12:11:01 +0900

    bugs fixed

--- src/Results.js
+++ src/Results.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import Phonetic from './Phonetic';
+import Definitions from './Definitions';
 import './Results.css';
 
 export default function Results(props) {
   const [showAllDefinitions, setShowAllDefinitions] = useState(false);
+  
 
-  const toggleDefinitions = (event) => {
-    event.preventDefault();
+  const toggleDefinitions = () => {
     setShowAllDefinitions(!showAllDefinitions);
-
   };
 
   if (props.results) {
@@ -16,7 +16,7 @@ export default function Results(props) {
     const meanings = props.results.meanings;
 
     return (
-      <div className="Results ">
+      <div className="Results">
         <div className="results-phonetic">
           <h1 className="results-word">{props.results.word}</h1>
           <Phonetic phonetic={firstPhonetic} />
@@ -28,24 +28,13 @@ export default function Results(props) {
           <h3>{meanings[0].partOfSpeech}</h3>
           <p>{meanings[0].definitions[0].definition}</p>
           {meanings.length > 1 && (
-            <a href="/" className="more-definitions-link" onClick={toggleDefinitions}>
+            <button className="more-definitions-button" onClick={toggleDefinitions}>
               Show more definitions
-            </a>
+            </button>
           )}
         </div>
-        
-        {showAllDefinitions && (
-          <div className="results-definitions">
-            {meanings.slice(2).map((meaning, index) => (
-              <div key={index}>
-                <h3>{meaning.partOfSpeech}</h3>
-                {meaning.definitions.map((definition, index) => (
-                  <p key={index}>{definition.definition}</p>
-                ))}
-              </div>
-            ))}
-          </div>
-        )}
+
+        {showAllDefinitions && <Definitions meanings={meanings.slice(1)} />}
       </div>
     );
   } else {
